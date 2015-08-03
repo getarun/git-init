56a8a4c28b5f3296ab69410d635c7caf23b84260
Zimmer-web <getarun@users.noreply.github.com>
2015-08-03 14:18:08 +0200

    Update and rename master.md to setup-git.sh

---
+++ setup-git.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+-*- coding: utf-8 -*-
+
+mkdir /home/pi/git-working-dir
+cd /home/pi/git-working-dir
+git config --global user.name "Zimmer-Pi"
+git config --global user.email zimmer@jzo.de
+git config --global --add merge.ff false

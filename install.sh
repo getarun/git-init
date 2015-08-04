e89020f48a533c98730bb6dd0429c6572cdb223c
Zimmer-web <getarun@users.noreply.github.com>
2015-08-04 09:39:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 
 sudo apt-get install git
 mkdir /home/pi/git-working-dir
-cd /home/pi/git-working-dir
-git config --global user.name "Zimmer-pi"
-git config --global user.email zimmer@jzo.de
-git config --global --add merge.ff false
+cp .gitconfig ~/.gitconfig
+
+#git config --global user.name "Zimmer-pi"
+#git config --global user.email zimmer@jzo.de
+#git config --global --add merge.ff false

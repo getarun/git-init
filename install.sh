208f896694d975e569d284fa8b4119734ba9a415
Zimmer-web <getarun@users.noreply.github.com>
2015-12-16 21:11:31 +0100

    Update install.sh
    added some coinfuguratuion lines

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ cp .gitconfig ~/.gitconfig
 #git config --global user.name "Zimmer-pi"
 #git config --global user.email zimmer@jzo.de
 #git config --global --add merge.ff false
+
+#git config --global push.default matching
